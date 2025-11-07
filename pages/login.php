5d41e673453aee6df39ead60a4f3e31155ad68d0
Cameron0305 <camcam030503@gmail.com>
2025-11-07 07:07:11 -0600

    Save login.html as php file - Cameron

---
+++ pages/login.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html> <!-- Declarition -->
+<html lang="en"> <!-- Root Element -->
+<head>
+    <meta charset="UTF-8"> <!-- Character set (supports most characters and symbols) -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/> <!-- Standard viewport (sets the width of the page to follow the screen-width of the device with intial zoom level) -->
+    <title>Log In — GPSense</title>
+
+    <link rel="stylesheet" href="/assets/css/login.css"> <!-- Links external CSS file-->
+</head>
+<body>
+    <div class="login-container"> <!-- Main login box -->
+        <img src="/assets/images/thelogo.png" alt="GPSense Logo" class="logo">
+        
+        <h2>Login to 
+            <span class="highlight">GPS<span>ense</span></span>
+        </h2>
+
+        <!-- Show error if login fails -->
+        <?php if (isset($_GET['error']) && $_GET['error'] == 1): ?>
+            <p class="error-message">Invalid username or password. Please try again</p>
+        <?php endif; ?> 
+
+        <!-- Login Form -->
+        <form action="/pages/loginhandler.php" method="POST" class="login-form">
+            <input type="text" name="username" placeholder="Username" required>
+            <input type="password" name="password" placeholder="Password" required>
+            <button type="submit">Login</button>
+        </form>
+
+        <a href="/pages/register.html" class="register-link">Don't have an account? Create one!</a>
+    </div>
+</body>
+<?php
+    include('/includes/footer.php')
+?>
+</html>
