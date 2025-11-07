303fa7aa4eeab4da135cdfd7dc013e31ec8a7257
Cameron0305 <camcam030503@gmail.com>
2025-11-07 06:42:45 -0600

    Login page css

---
+++ includes/footer.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html> <!-- Declaration -->
+<html lang="en"> <!-- Root Element -->
+<head>
+    <meta charset="UTF-8"> <!-- Character set (supports most characters and symbols) -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/> <!-- Standard viewport (sets the width of the page to follow the screen-width of the device with initial zoom level) -->
+    <title>Copyright Placement</title>
+    <style type="text/css">
+        footer {
+            position: fixed; /* Keeps it at the same position even when scrolling */
+            bottom: 40px;    /* 20px from the bottom */
+            right: 60px;     /* 20px from the right */
+            font-size: 14px; /* Adjust font size as needed */
+        }
+    </style>
+</head>
+    
+<body>
+   <footer>
+          &copy; 2025
+   </footer>
+</body>
+</html>
