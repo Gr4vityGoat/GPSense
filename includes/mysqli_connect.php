de7fcb1807c221040e4ff1714a18a6d7bf3b5faa
Cameron0305 <camcam030503@gmail.com>
2026-02-25 15:13:20 -0600

    db_name change

--- includes/mysqli_connect.php
+++ includes/mysqli_connect.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $db_user = "infost490fa2507_ryan";
 $db_pass = "BloodClotGPS01";
-$db_name = "infost490fa2507_gpsense";
+$db_name = "gpsense";
 
 $mysqli = new mysqli($host, $db_user, $db_pass, $db_name);
 
